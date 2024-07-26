8b0e418f0f98e9bdf1a42dd6ba6b5ee95b8bfe4c
Alvin Dandeebo <161020490+dsalvin@users.noreply.github.com>
2024-07-26 20:33:45 +0300

    update

--- main.py
+++ main.py
@@ -1,14 +1,48 @@
 from tkinter import *
-
+from tkinter import messagebox
+import random
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+nr_letters = random.randint(8, 10)
+nr_symbols = random.randint(2, 4)
+nr_numbers = random.randint(2, 4)
+
+password_list = []
+
+for char in range(nr_letters):
+  password_list.append(random.choice(letters))
+
+for char in range(nr_symbols):
+  password_list += random.choice(symbols)
+
+for char in range(nr_numbers):
+  password_list += random.choice(numbers)
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
-    with open("passwords.txt", "a") as file:
-        file.write(f"{website_entry.get()} | {user_entry.get()} | {password_entry.get()}  \n")
-    website_entry.delete(0, END)
-    password_entry.delete(0, END)
+
+    if len(website_entry.get() ) == 0 or len(password_entry.get()) == 0:
+        messagebox.showerror(title="Oops", message="Please don't leave any fields empty")
+    else:
+        is_ok = messagebox.askokcancel(title=website_entry.get(), message=f"These are the details entered: "
+                f"\nEmail: {user_entry.get()} \nPassword: {password_entry.get()} \nIs it ok to  save?")
+        if is_ok:
+            with open("passwords.txt", "a") as file:
+                file.write(f"{website_entry.get()} | {user_entry.get()} | {password_entry.get()}  \n")
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
